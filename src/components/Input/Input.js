3df47445dac25e7ef4dd06988c694be35dd6ebb2
John Mattew Y. Flores <flores.jmy@AD.SPROBE.PH>
2019-08-01 08:26:29 +0800

    Added new documentation styles

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,23 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-  /**
-   * @property {string} className is used for input class
-   * @property {string} type is used for input type
-   * @property {object} styles is used for inline styles
-   * @property {string} value is used for input value
-   * @property {func} onChange is used for overriding input function
-   */
+  /** @property {string} className is used for input class */
   className: PropTypes.string,
+
+  /** @property {string} type is used for input type */
   type: PropTypes.string,
+
+  /** @property {string} placeholder is used for input placeholder */
+  placeholder: PropTypes.string,
+
+  /** @property {object} styles is used for inline styles */
   styles: PropTypes.object,
+
+  /** @property {string} value is used for input value */
   value: PropTypes.string.isRequired,
+  
+  /** @property {func} onChange is used for overriding input function */
   onChange: PropTypes.func.isRequired
 
 }
 
 const defaultProps = {
   className: "",
+  placeholder: "",
   type: "text",
   styles: {},
 }
@@ -36,6 +42,7 @@ class Input extends Component {
         className={this.props.className}
         styles={this.props.styles}
         name={this.props.name}
+        placeholder={this.props.placeholder}
         value={this.props.value}
         type={this.props.type}
         onChange={this.props.onChange}
